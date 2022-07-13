7e10ebf87cd333715e921929e1ce39d458b09dfd
Denilsson de Sousa <denilsson.d.sousa@gmail.com>
2022-07-13 10:18:15 -0400

    ajustes

--- client/src/components/home/Section6.vue
+++ client/src/components/home/Section6.vue
@@ -9,12 +9,14 @@
     </div>
 
     <div class="height_section2 row">
-      <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 column justify-center items-start q-pa-xl text-weight-bolder text-h5">
-        <div class="bg-secondary text-white q-px-md">Campito</div>
-        <div>2-4 años</div>
-        <q-chip color="secondary" text-color="white" icon-right="add" clickable>
-          Conocer
-        </q-chip>
+      <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 contenedor_section6 q-pa-xl text-weight-bolder">
+        <div class="text_responsive">
+          <div class="bg-secondary text-white q-px-md">Campito</div>
+          <div>2-4 años</div>
+          <q-chip color="secondary" text-color="white" icon-right="add" clickable>
+            Conocer
+          </q-chip>
+        </div>
       </div>
       <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
         <div class="paralax_section block1">
@@ -24,12 +26,14 @@
     </div>
 
     <div class="height_section2 row">
-      <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 column justify-center items-start q-pa-xl text-weight-bolder text-h5">
-        <div>Educación <br><b class="bg-primary text-white q-px-md">básica</b></div>
-        <div>5-15 años</div>
-        <q-chip color="primary" text-color="white" icon-right="add" clickable>
-          Conocer
-        </q-chip>
+      <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 contenedor_section6 q-pa-xl text-weight-bolder">
+        <div class="text_responsive">
+          <div>Educación <br><b class="bg-primary text-white q-px-md">básica</b></div>
+          <div>5-15 años</div>
+          <q-chip color="primary" text-color="white" icon-right="add" clickable>
+            Conocer
+          </q-chip>
+        </div>
       </div>
       <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
         <div class="paralax_section block2">
@@ -39,12 +43,14 @@
     </div>
 
     <div class="height_section2 row">
-      <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 column justify-center items-start q-pa-xl text-weight-bolder text-h5">
-        <div class="bg-accent text-white q-px-md">Bachillerato</div>
-        <div>16-18 años</div>
-        <q-chip color="accent" text-color="white" icon-right="add" clickable>
-          Conocer
-        </q-chip>
+      <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 contenedor_section6 q-pa-xl text-weight-bolder">
+        <div class="text_responsive">
+          <div class="bg-accent text-white q-px-md">Bachillerato</div>
+          <div>16-18 años</div>
+          <q-chip color="accent" text-color="white" icon-right="add" clickable>
+            Conocer
+          </q-chip>
+        </div>
       </div>
       <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
         <div class="paralax_section block3">
