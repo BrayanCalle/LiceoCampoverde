a7385dda1fb53fcc3b65a0ab8d155b0c84e69a29
Denilsson de Sousa <denilsson.d.sousa@gmail.com>
2022-07-15 08:32:04 -0400

    ajustes de blog

--- client/src/components/home/Section8.vue
+++ client/src/components/home/Section8.vue
@@ -5,11 +5,29 @@
       <div class="bg-primary q-px-md">eventos</div>
     </div>
 
-    <div class="row justify-around" style="padding-left:10em; padding-right:10em">
+    <!-- Solo se muestra en web -->
+    <div class="row justify-around web_element" style="padding-left:10em; padding-right:10em">
       <div v-for="(item, index) in data" :key="index"
-        class="col-xs-9 col-sm-5 col-md-3 col-lg-3 row justify-center" style="padding:1em">
+        class="col-xs-12 col-sm-6 col-md-3 col-lg-3 row justify-center padding_blog">
         <blog-card :data="item" />
       </div>
+    </div>
+
+    <!-- Solo se muestra en movil -->
+    <div class="q-py-md q-px-xl mobile_element">
+      <q-carousel
+        v-model="slide"
+        swipeable
+        animated
+        control-color="grey-8"
+        navigation
+        padding
+        height="100%"
+      >
+        <q-carousel-slide :name="index + 1" v-for="(item, index) in data" :key="index">
+          <blog-card :data="item" />
+        </q-carousel-slide>
+      </q-carousel>
     </div>
 
     <div class="row justify-center q-py-lg">
@@ -27,6 +45,7 @@ export default {
   },
   data () {
     return {
+      slide: 1,
       data: [
         {
           id: '1',
